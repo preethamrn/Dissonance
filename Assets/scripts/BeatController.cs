ec3fcc986465a2e294d2394657ec3b5a8e1685c9
Matthew Kuzdal <mkuzdal@gmail.com>
2016-10-22 03:39:58 -0700

    Updated BeatController

--- Assets/scripts/BeatController.cs
+++ Assets/scripts/BeatController.cs
@@ -1,31 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BeatController: MonoBehaviour {
+public class BeatController : MonoBehaviour {
 
 	const float BPM = 120f;
+	const float beatCooldown = 60 / BPM;
+	const float buffer = 0.4f;
 
-	float beatCooldown = 60 / BPM;
-	float beatCooldownLeft = 0f;
+
+	float beatCooldownLeft;
+	float playerMovedCD;
+
+	int laneToMove;
+	int beat;
+	int beatDelay;
+
+	bool playerDied;
+
+	// MUSICAL BEATS OCCUR EVERY BEAT - (BUFFER / 2) 
 
 	// Use this for initialization
 	void Start () {
-	
+
+		beatCooldownLeft = buffer;
+		playerMovedCD = 0f;
+		beat = 0;
+		beatDelay = 10;
+		playerDied = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 		beatCooldownLeft -= Time.deltaTime;
-		if(beatCooldownLeft <= 0) {
+		playerMovedCD -= Time.deltaTime;
 
-			// Do all on Beat tasks
-			
-
-			
-
-			Debug.Log("on beat");
+		if (beat < beatDelay && beatCooldownLeft <= 0) {
 			beatCooldownLeft = beatCooldown;
+			beat++;
+			Debug.Log("Delay");
+		}
+
+		else if (beat >= beatDelay) {
+
+			if (playerDied == false && playerMovedCD >= buffer) {
+				// the player lost
+				Debug.Log("Player lost due to clicking too early!");
+				playerDied = true;
+			}
+
+			else if (beatCooldownLeft <= 0) {
+
+				if (playerMovedCD < 0 - beatCooldown) {
+					// the player lost
+					Debug.Log("Player Lost due to not clicking!");
+				}
+
+				Debug.Log("On Beat");
+				// MOVE ALL OBJECTS
+
+
+				
+
+				beatCooldownLeft = beatCooldown;
+				beat++;
+				playerDied = false;
+			}
 		}
 	}
+
+	public void input(int lane) {
+
+		if (playerMovedCD <= 0) {
+			laneToMove = lane;
+			playerMovedCD = beatCooldownLeft;
+		}
+	}
+
+
 }
