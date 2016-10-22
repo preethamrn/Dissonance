a90c09c854315337f05ca83071d8a22ef5db212e
Preetham N <preetham.in.1997@gmail.com>
2016-10-21 23:02:19 -0700

    Added lane clicking

--- Assets/scripts/LaneClick.cs
+++ Assets/scripts/LaneClick.cs
@@ -3,19 +3,37 @@ using System.Collections;
 
 public class LaneClick : MonoBehaviour {
 
-    int screenSize;
+    int screenSizeX, screenSizeY;
+    public Transform go;
 
     // Use this for initialization
     void Start () {
-        screenSize = Screen.currentResolution.width;
-	}
+        /*
+        //For Desktop X,Y is switched for getting vector... Don't ask why.
+        screenSizeX = Screen.currentResolution.height;
+        screenSizeY = Screen.currentResolution.width;
+        */
+
+        screenSizeX = Screen.currentResolution.width;
+        screenSizeY = Screen.currentResolution.height;
+        Debug.Log(screenSizeX.ToString() + " " + screenSizeY.ToString());
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        /*if (Input.GetMouseButtonDown(0)) {
+            Vector2 touch = Input.mousePosition;
+            Debug.Log(touch);
+            int lane = (int)touch.x / (screenSizeX / 5);
+            Instantiate(go, new Vector3(18*touch.x/screenSizeX-9, 32*touch.y/screenSizeY-16, 0), Quaternion.identity);
+            Debug.Log("Hit Lane" + lane);
+        }*/
+
         Touch[] touches = Input.touches;
         if (touches.Length > 0) {
             Touch touch = touches[0];
-            int lane = (int)touch.position.x / (screenSize / 5);
+            int lane = (int)touch.position.x / (screenSizeX / 5);
+            Instantiate(go, new Vector3(18*touch.position.x/screenSizeX-9, 32*touch.position.y/screenSizeY-16, 0), Quaternion.identity);
             Debug.Log("Hit Lane" + lane);
         }
     }
