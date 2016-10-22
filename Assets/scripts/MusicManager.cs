898e94af40a2ab4f014d7e836dc0f7231bbae915
Jaron Maurice Mink <jaronmink@gmail.com>
2016-10-22 03:47:29 -0700

    can't add scripts to camera, feels bad

---
+++ Assets/scripts/MusicManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager
+{
+    AudioSource m_song;
+    ulong m_delay;
+    MusicManager(AudioSource song)
+    {
+        m_song = song
+
+
+    }
+
+    void playSong(int songNum)
+    {
+
+        m_song.Play(m_delay);
+    }
+
+    //delay is measured in seconds
+    void playSong(int songNum, ulong delay)
+    { m_song.Play(delay); }
+
+    void stopSong(int songNum)
+    { m_song.Stop(); }
+}
