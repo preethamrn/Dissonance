86e15f9bae257df803ec387c41c000a25b7098f0
Jaron Maurice Mink <jaronmink@gmail.com>
2016-10-21 22:54:52 -0700

    Sick bullet movement

---
+++ Assets/scripts/BulletScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+    float moveLength;
+   // bool enemyShot = false;
+    // Use this for initialization
+    void Start()
+    {
+        moveLength = Screen.currentResolution.height /7;
+    }
+
+    // Update is called once per frame
+    //do nothing unless triggered by the game controller
+    void Update()
+    {
+       
+    }
+        //maybe send message that a person won
+    
+        //if leaves the screen kill, else move
+    public void move()
+      {
+       
+        Vector3 position = this.transform.position;
+        if (position.y == Screen.currentResolution.height) 
+            Destroy(gameObject);
+        position.y += moveLength;
+        this.transform.position = position;
+      }
+ }
---
+++ Assets/scripts/HealthScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+    public bool alive = true;
+    public bool isEnemy = true;
+   
+	// Use this for initialization
+    
+
+    public void kill()
+    {
+        alive = false;
+        Destroy(gameObject);
+    }
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
