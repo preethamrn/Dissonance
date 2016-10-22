1e415909e8951802e250e7166a2b99309fdabb9c
Matthew Kuzdal <mkuzdal@gmail.com>
2016-10-21 21:56:29 -0700

    Added Beat Controller script

--- Assets/scripts/BeatController.cs
+++ Assets/scripts/BeatController.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BeatController : MonoBehaviour {
+public class BeatController: MonoBehaviour {
+
+	const float BPM = 120f;
+
+	float beatCooldown = 60 / BPM;
+	float beatCooldownLeft = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -11,5 +16,11 @@ public class BeatController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		beatCooldownLeft -= Time.deltaTime;
+		if(beatCooldownLeft >= 0) {
+			// Do all on Beat tasks
+			Debug.Log("on beat");
+			beatCooldownLeft = beatCooldown;
+		}
 	}
 }
