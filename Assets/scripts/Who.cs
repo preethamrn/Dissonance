f43cc1d2f75318ad3fddc50a47dc3485532eeca6
Allen Zhou <Zhouball@gmail.com>
2016-10-22 12:27:53 -0700

    Got basic networking

---
+++ Assets/scripts/Who.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Who : MonoBehaviour {
+
+	GameObject me;
+	GameObject notme;
+
+	public void SetMe(GameObject go) {
+		me = go;
+		Debug.Log("Set Me!");
+	}
+
+	public void SetNotMe(GameObject go) {
+		notme = go;
+		Debug.Log("Set Not Me!");
+	}
+
+	public GameObject GetMe() {
+		return me;
+	}
+
+	public GameObject GetNotMe() {
+		return notme;
+	}
+}
