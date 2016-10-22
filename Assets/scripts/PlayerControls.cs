645a76fea17c3fedb3747c9e804f71973f916549
Preetham N <preetham.in.1997@gmail.com>
2016-10-22 00:47:19 -0700

    Started PlayerControls script and added player prefab

---
+++ Assets/scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+    int lane=3;
+    BulletScript bulletScript;
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        bulletScript = FindObjectOfType<BulletScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void move(int newLane) {
+        if (newLane > lane) lane++;
+        else if (newLane < lane) lane--;
+
+        bulletScript.createBullet(newLane);
+
+        
+    }
+}
