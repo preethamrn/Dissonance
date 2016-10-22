416478d0fce423a115917d4a0060f2f6c08cc0ff
Jaron Maurice Mink <jaronmink@gmail.com>
2016-10-22 02:34:42 -0700

    made audio class

---
+++ Assets/scripts/Music Manager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+    public class MusicManager
+    {
+       AudioSource m_song;
+       ulong m_delay;
+    MusicManager(AudioSource song, ulong delay)
+    {
+        m_song = song;
+        m_delay = delay;
+    }
+
+        void playSong()
+          {m_song.Play(m_delay);}
+
+        //delay is measured in seconds
+        void playSong(ulong delay)
+          {m_song.Play(delay);}
+
+        void stopSong()
+          {m_song.Stop();}
+    }
