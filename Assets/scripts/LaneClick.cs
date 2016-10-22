89466bde944b7ce40bbfbb6088e995a7dde24f5d
Preetham N <preetham.in.1997@gmail.com>
2016-10-22 01:39:07 -0700

    Modified player movement to work for Android

--- Assets/scripts/LaneClick.cs
+++ Assets/scripts/LaneClick.cs
@@ -8,31 +8,32 @@ public class LaneClick : MonoBehaviour {
     void Start () {
         
         //For Desktop X,Y is switched for getting vector... Don't ask why.
-        screenSizeX = Screen.currentResolution.height;
-        screenSizeY = Screen.currentResolution.width;
+        //screenSizeX = Screen.currentResolution.height;
+        //screenSizeY = Screen.currentResolution.width;
 
-        //screenSizeX = Screen.currentResolution.width;
-        //screenSizeY = Screen.currentResolution.height;
+        screenSizeX = Screen.currentResolution.width;
+        screenSizeY = Screen.currentResolution.height;
         Debug.Log(screenSizeX.ToString() + " " + screenSizeY.ToString());
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0)) {
+        /*if (Input.GetMouseButtonDown(0)) {
             Vector2 touch = Input.mousePosition;
             Debug.Log(touch);
             int lane = (int)touch.x / (screenSizeX / 5);
             //Instantiate(go, new Vector3(18*touch.x/screenSizeX-9, 32*touch.y/screenSizeY-16, 0), Quaternion.identity);
             Debug.Log("Hit Lane" + lane);
             FindObjectOfType<PlayerControls>().move(lane);
-        }
+        }*/
 
-        /*Touch[] touches = Input.touches;
+        Touch[] touches = Input.touches;
         if (touches.Length > 0) {
             Touch touch = touches[0];
             int lane = (int)touch.position.x / (screenSizeX / 5);
             //Instantiate(go, new Vector3(18*touch.position.x/screenSizeX-9, 32*touch.position.y/screenSizeY-16, 0), Quaternion.identity);
             Debug.Log("Hit Lane" + lane);
-        }*/
+            FindObjectOfType<PlayerControls>().move(lane);
+        }
     }
 }
