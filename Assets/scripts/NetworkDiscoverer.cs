caae2129acac031d91acbbbe31c4a84b095266b0
Preetham N <preetham.in.1997@gmail.com>
2016-10-23 03:39:24 -0700

    Added incomplete network stuff

---
+++ Assets/scripts/NetworkDiscoverer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkDiscoverer : NetworkDiscovery {
+
+    public override void OnReceivedBroadcast(string fromAddress, string data) {
+        NetworkManager.singleton.networkAddress = fromAddress;
+        FindObjectOfType<Networker>().addServer(fromAddress);
+    }
+
+    public void receivedBroadcast() {
+        NetworkManager.singleton.StartClient();
+    }
+
+    public void sendBroadcast() {
+        NetworkManager.singleton.StartClient();
+    }
+}
